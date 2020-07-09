4f7ae6515fd364f374bd0076fbd1f3509fd47d4f
derha <hallerd@mail.gvsu.edu>
2020-07-08 20:37:02 -0400

    Created basic classes to handle the poll room, as well as individual polls and their responses and participants.

---
+++ poll.py
@@ -0,0 +1,55 @@
+class PollRoom:
+
+    def __init__(self, owner, room_id):
+        self.polls = None
+        self.owner = owner
+        self.roomID = room_id
+
+
+class Poll:
+    def __init__(self, room, question, response_type, display_type, active=False):
+        self.room = room
+        self.question = question
+        self.response_type = response_type
+        self.display_type = display_type
+        self.active = active
+
+
+class PollParticipant:
+    def __init__(self, nickname, room):
+        self.nickname = nickname
+        assert isinstance(room, PollRoom)
+        self.room = room
+
+    @property
+    def room(self):
+        return self.room
+
+    @property
+    def nickname(self):
+        return self.nickname
+
+
+class PollResponse:
+    def __init__(self, participant, response):
+        """
+        Constructs all the necessary attributes for a poll response
+
+        :parameter:
+        __________
+            :type participant: PollParticipant
+                response's owner
+            :type response: str
+                response to the question
+        """
+        assert isinstance(participant, PollParticipant)
+        self.participant = participant
+        self.response = response
+
+    @property
+    def participant(self):
+        """
+        Returns the participant who made this response
+        :return: participant: PollParticipant
+        """
+        return self.participant
