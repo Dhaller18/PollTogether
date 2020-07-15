67338434f95c05212a4f4cec580bcb0e3a5a6273
GWEbe <gwebelingiv@gmail.com>
2020-07-14 20:31:05 -0400

    Added test for clicking an answer, but it currently does not pass. Code coverage is 55%.

--- test_UnitTests.py
+++ test_UnitTests.py
@@ -1,26 +1,48 @@
 import os
 import unittest
+
+from flask import url_for
+
 from app import app
 
 
 class RoutingTests(unittest.TestCase):
-    # function to set up testing connection
+
     def setUp(self):
         app.config["TESTING"] = True
         app.config["DEBUG"] = True
         self.app = app.test_client()
 
-    # function to teardown connection after testing
     def tear_down(self):
         pass
 
-    def test_homepage(self):
+    def test_homePage(self):
         response = self.app.get('/', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
 
-    def test_aboutpage(self):
-        response = self.app.get('/', follow_redirects=True)
+    def test_questionPage(self):
+        response = self.app.get('/question/', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
+
+
+class TestQuestions(unittest.TestCase):
+
+    def setUp(self):
+        app.config["TESTING"] = True
+        app.config["DEBUG"] = True
+        self.app = app.test_client()
+
+    def tear_down(self):
+        pass
+
+    # assumes the current question is
+    def test_questionPage(self):
+        response = self.app.get('/question/', follow_redirects=True)
+        ans = {'A': 'A'}
+        data = dict(ans=ans)
+        self.app.post('/question/', ans=data, follow_redirects=True)
+
+        assert response.request.path == url_for('/question/#')
 
 
 if __name__ == "__main__":
