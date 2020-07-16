e772a774068f7e29f47f7b9fa5139b1f9cd08686
nikkormagic <ebelingg@mail.gvsu.edu>
2020-07-15 22:53:32 -0400

    Added unittests for app.py. Coverage is 83% with just one function being missed.

--- tests/test_UnitTests.py
+++ tests/test_UnitTests.py
@@ -1,10 +1,9 @@
-import os
-import flask
-import uuid
 import unittest
 import app as site
-from flask import url_for
-from app import app
+from app import app, socketio
+from flask_socketio import SocketIO, emit, join_room
+
+pollroom = 'kdrl'
 
 
 class RoutingTests(unittest.TestCase):
@@ -26,12 +25,12 @@ class RoutingTests(unittest.TestCase):
         self.assertTrue(b'Would you like to:' in response.data)
 
     def test_questionPage(self):
-        response = self.app.get('/question/', follow_redirects=True)
+        response = self.app.get('/room/kdrl', follow_redirects=True)
         self.assertEqual(response.status_code, 200, "questions page could not be loaded")
 
     def test_questionPageLoaded(self):
-        response = self.app.get('/question/', content_type='html/text')
-        self.assertTrue(b'What option do you think is correct?' in response.data)
+        response = self.app.get('/room/kdrl', content_type='html/text')
+        self.assertTrue(b'Welcome to Room: ' in response.data)
 
     def test_joinRoom(self):
         response = self.app.get('/joinRoom/', content_type='html/text')
@@ -41,42 +40,59 @@ class RoutingTests(unittest.TestCase):
         response = self.app.get('/joinRoom/', content_type='html/text')
         self.assertTrue(b'Room ID:' in response.data)
 
+    def test_results(self):
+        response = self.app.get('/room/kdrl/results/9', content_type='html/text')
+        self.assertEqual(response.status_code, 200, "joinRoom page could not be loaded")
 
-class TestQuestions(unittest.TestCase):
+    def test_resultsLoaded(self):
+        response = self.app.get('/room/kdrl/results/9', content_type='html/text')
+        self.assertTrue(b'Results to question' in response.data)
+
+    def test_resultsWrongPollID(self):
+        response = self.app.get('/room/kdrl/results/1', content_type='html/text', follow_redirects=True)
+        self.assertTrue(b'No polls yet...' in response.data)
+
+
+class TestCreatePoll(unittest.TestCase):
 
     def setUp(self):
         app.config["TESTING"] = True
         app.config["DEBUG"] = True
         self.app = app.test_client()
+        self.client = socketio.test_client(app, flask_test_client=self.app)
 
     def tearDown(self):
+        client = socketio.test_client(app, flask_test_client=self.app)
         pass
 
-    def test_add_answers_NoData(self):
-        with app.test_request_context('/question/'):
-            mydict = site.add_answers('A')
-            self.assertIsNotNone(mydict, "Default dictionary was not created")
+    def test_formatPoll(self):
+        expected_poll = {'A': 0, 'B': 0, 'C': 0, 'D': 0}
+        new_poll = site.Poll(room='kdrl', question='Test question',
+                             choice1='testA', choice2='testB',
+                             choice3='testC', choice4='testD',
+                             response1=0, response2=0, response3=0,
+                             response4=0)
+        new_poll = site.formatPoll(new_poll)
+        self.assertEqual(expected_poll, new_poll, "Poll was not formatted as expected")
 
-    def test_add_answers_NoData_AnswerGiven(self):
-        with app.test_request_context('/question/'):
-            mydict = site.add_answers('A')
-            expectedDict = {'A': 1, 'B': 0, 'C': 0, 'D': 0}
-            self.assertEqual(expectedDict, mydict, "Answer not counted")
+    def test_createQuestion(self):
+        with app.test_request_context('/room/kdrl/'):
+            new_poll = {'r_id': 'kdrl', 'Q': 'Test question', 'A': 'testA', 'B': 'testB', 'C': 'testC',
+                        'D': 'testD', 'choice1': 0, 'choice2': 0, 'choice3': 0, 'choice4': 0}
+            response = self.app.get('/room/kdrl', follow_redirects=True)
+            self.client.emit('makepoll', new_poll)
+            self.assertTrue(b'Current Polls:' in response.data)
 
-    # assumes the current question has answer 'A'
-    def test_questionPageWithData(self):
-        with app.test_request_context('/question/'):
-            response = self.app.post(
-                '/question/',
-                data={'ans': 'A'},
-                follow_redirects=True
-            )
-        self.assertTrue(b'Results to question' in response.data)
+# Due to the lack of a drop function, this method requires the room to be made new before running the test. Uncomment
+# test before running the final test.
+    def test_createNewPoll(self):
+        with app.test_request_context('/room/kdro/'):
+            new_poll = {'room_id': 'kdro'}
 
-    def test_resultsNoData(self):
-        response = self.app.get('/results/', follow_redirects=True)
-        self.assertTrue(b'What option do you think is correct?' in response.data, "Was not redirected to questions page")
+            response = self.app.get('/room/kdro', follow_redirects=True)
 
+            self.client.emit('create', new_poll)
+            self.assertTrue(b'Current Polls:' in response.data)
 
 
 class TestRooms(unittest.TestCase):
@@ -85,19 +101,21 @@ class TestRooms(unittest.TestCase):
         app.config["TESTING"] = True
         app.config["DEBUG"] = True
         self.app = app.test_client()
+        self.client = socketio.test_client(app, flask_test_client=self.app)
 
     def tearDown(self):
+        client = socketio.test_client(app, flask_test_client=self.app)
         pass
 
-    # expects there to be a room with the ID XEOW
+    # expects there to be a room with the ID kdrl
     def test_roomJoined(self):
         with app.test_request_context('/joinRoom'):
             response = self.app.post(
                 '/joinRoom/',
-                data={'roomName': 'XEOW'},
+                data={'roomName': 'kdrl'},
                 follow_redirects=True
             )
-        self.assertTrue(b'Welcome to Room: XEOW' in response.data)
+        self.assertTrue(b'Welcome to Room: kdrl' in response.data)
 
 
 if __name__ == "__main__":
