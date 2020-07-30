2e4994d2aa4a683324d9aeb1406ffa79f2dc03d0
derha <hallerd@mail.gvsu.edu>
2020-07-29 20:43:01 -0400

    Made polls for the room generate with the template when they are active and the user hasn't answered them yet. Added new database model to keep track of polls that the user has answered.

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import uuid
 
-from flask import Flask, redirect, url_for, render_template, request, session
+from flask import Flask, redirect, url_for, render_template, request, session, jsonify
 from flask_socketio import SocketIO, emit, join_room
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = "aklsdfjlksdahfkVdHDKHlkdsjfSDkfj323KDSFhk"
 socketio = SocketIO(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pollTogether.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pollTogether3.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -21,6 +21,10 @@ class Room(db.Model):
 class Poll(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     room = db.Column(db.String(10), nullable=False)
+    active = db.Column(db.Boolean, default=True, nullable=False)
+    poll_type = db.Column(db.String(20), default="Pie", nullable=False)
+    response_type = db.Column(db.String(20), default="mc", nullable=False)
+    show_results = db.Column(db.Boolean, default=True, nullable=False)
     question = db.Column(db.String(100))
     choice1 = db.Column(db.String(100))
     choice2 = db.Column(db.String(100))
@@ -31,9 +35,36 @@ class Poll(db.Model):
     response3 = db.Column(db.Integer, nullable=False)
     response4 = db.Column(db.Integer, nullable=False)
 
+    def serialize(self):
+        return {
+            'id' : self.id,
+            'room' : self.room,
+            'active' : self.active,
+            'poll_type' : self.poll_type,
+            'response_type' : self.response_type,
+            'show_results' : self.show_results,
+            'question' : self.question,
+            'choice1' : self.choice1,
+            'choice2' : self.choice2,
+            'choice3' : self.choice3,
+            'choice4' : self.choice4,
+            'response1' : self.response1,
+            'response2' : self.response2,
+            'response3' : self.response3,
+            'response4' : self.response4
+        }
+
+class Participated(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    poll_id = db.Column(db.Integer, nullable=False)
+    uid = db.Column(db.String(50), nullable=False)
+
 
 @app.route('/', methods=["POST", "GET"])
 def home():
+    session.permanent = True
+    if 'uid' not in session:
+        session['uid'] = str(uuid.uuid1())
     session["user"] = "User001"
     poll_user = session["user"]
     return render_template("home.html", user=poll_user,
@@ -42,6 +73,8 @@ def home():
 
 @app.route('/joinRoom/', methods=["POST", "GET"])
 def join_existing():
+    if 'uid' not in session:
+        session['uid'] = str(uuid.uuid1())
     if request.method == "POST":
         room = request.form.get('roomName')
         if db.session.query(db.exists().where(Room.name == room)).scalar():
@@ -73,8 +106,18 @@ def formatPoll(poll):
 def poll_room(room_id):
     if 'uid' not in session:
         session['uid'] = str(uuid.uuid1())
+    participated = []
+    for entry in Participated.query.filter_by(uid=session['uid']).all():
+        participated.append(entry.poll_id)
+
+    room_polls = Poll.query.filter_by(room=room_id).all()
+    all_polls = []
+    for poll in room_polls:
+        all_polls.append(poll.serialize())
     return render_template("pollRoom.html", user=session['uid'],
-                           room=Room.query.filter_by(name=room_id).first())
+                           room=Room.query.filter_by(name=room_id).first(),
+                           polls=all_polls,
+                           answered_polls=participated)
 
 
 @socketio.on('makepoll')
@@ -86,13 +129,8 @@ def on_new_poll(data):
                     response4=0)
     db.session.add(new_poll)
     db.session.commit()
-    poll_data = {'id': new_poll.id,
-                 'room': new_poll.room,
-                 'question': new_poll.question,
-                 'choice1': new_poll.choice1,
-                 'choice2': new_poll.choice2,
-                 'choice3': new_poll.choice3,
-                 'choice4': new_poll.choice4}
+    add_participated(new_poll.id)
+    poll_data = new_poll.serialize()
     emit('redirect', {'url': url_for("results", room_id=new_poll.room,
                                      poll_id=new_poll.id)})
     emit('newPoll', poll_data, room=new_poll.room)
@@ -116,6 +154,7 @@ def on_join(data):
 
 @socketio.on('pollResponse')
 def on_response(data):
+    add_participated(data['poll_id'])
     poll = Poll.query.filter_by(id=data['poll_id']).first()
     choice = data['response']
     if choice == 'A':
@@ -127,6 +166,14 @@ def on_response(data):
     elif choice == 'D':
         poll.response4 = Poll.response4 + 1
     db.session.commit()
+
+
+def add_participated(poll_id):
+    if 'uid' not in session:
+        session['uid'] = str(uuid.uuid1())
+    participated_poll = Participated(poll_id=poll_id, uid=session['uid'])
+    db.session.add(participated_poll)
+    db.session.commit
 
 
 if __name__ == '__main__':
